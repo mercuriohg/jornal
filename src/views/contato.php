9190c55f61de684c71d43008837770ab1f9bf3ba
Arthur Gomes <gomesarthursevero6@gmail.com>
2026-06-28 23:18:53 -0300

    criando a página contato.php

---
+++ src/views/contato.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contatos do Grêmio</title>
+    <link rel="stylesheet" href="/assets/style/index.css">
+    <link
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css"
+      integrity="sha512-2SwdPD6INVrV/lHTZbO2nodKhrnDdJK9/kg2XD1r9uGqPo1cUbujc+IYdlYdEErWNu69gVcYgdxlmVmzTWnetw=="
+      crossorigin="anonymous"
+      referrerpolicy="no-referrer"
+    />
+    <link rel="icon" href="/assets/img/malal_icon.ico" type="image/x-icon">
+</head>
+<body>
+    <?php include 'components/header.php'; ?>
+    <?php include 'components/sidebar.php'; ?>
+     <main id="contate">
+        <h2>Formas de mensagem do Grêmio Estudantil</h2>
+     </main>
+    <?php include 'components/footer.php'; ?>
+
+
+</body>
+</html>
