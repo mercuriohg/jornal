2ff0e09c4137784eb1442f810d0b87649a902325
Arthur Gomes <gomesarthursevero6@gmail.com>
2026-07-02 23:12:06 -0300

    adicionando o script da sidebar e a acessibilidade do gov.br

--- src/views/contato.php
+++ src/views/contato.php
@@ -21,6 +21,28 @@
         <h2>Formas de mensagem do Grêmio Estudantil</h2>
      </main>
     <?php include 'components/footer.php'; ?>
+    <script> 
+        const menuToggle = document.getElementById('menu-toggle');
+        const sidebar = document.getElementById('sidebar');
+
+        menuToggle.addEventListener('click', () => {
+            sidebar.classList.toggle('active');
+            
+            // Opcional: Transforma o ícone de barras em um "X" ao abrir
+            menuToggle.classList.toggle('fa-bars');
+            menuToggle.classList.toggle('fa-times');
+        });
+    </script>
+    <div vw class="enabled">
+    <div vw-access-button class="active"></div>
+    <div vw-plugin-wrapper>
+        <div class="vw-plugin-top-wrapper"></div>
+    </div>
+    </div>
+    <script src="https://vlibras.gov.br/app/vlibras-plugin.js"></script>
+    <script>
+    new window.VLibras.Widget('https://vlibras.gov.br/app');
+    </script>
 
 
 </body>
