999db8644cdaf954b28f8980718c7cf160a23615
Arthur Gomes <gomesarthursevero6@gmail.com>
2026-07-04 02:33:02 -0300

    mudando elementos do footer

--- src/views/components/footer.php
+++ src/views/components/footer.php
@@ -1,18 +1,18 @@
 <footer id="footer-main">
-    <div class="container-footer">
-        <div id="local">
-            <img src="/assets/img/ldm-notice.png" alt="Logo do Grêmio Estudantil - Legado de Malala" width="200px" height="200px">
+    <div id="content-principal-foot">
+        <div class="container-footer">
+            <div id="local">
+                <img src="/assets/img/ldm-notice.png" alt="Logo do Grêmio Estudantil - Legado de Malala" width="200px" height="200px">
+            </div>
+            <div id="contato">
+               
+            </div>
         </div>
-        <div id="contato">
-           <h4> Instituto Federal de Educação, Ciência e Tecnologia do Rio Grande do Sul – Campus Rolante </h4>
-            <address> Rodovia RS-239, Km 68, Nº 3505 (Estrada Taquara/Rolante) | CEP: 95690-000 | Rolante/RS Caixa Postal 118 (correspondência) | Telefone: (51) 3547-9600 </address>
-        </div>
+        <ul id="footer-links-socials">
+            <li class="li-foot"><a href="/contato"><i class="fa-solid fa-inbox"></i>Tire suas dúvidas</a></li>
+            <li class="li-foot"><a href="https://www.instagram.com/gremio_estudantil.if/" target="_blank"><i class="fab fa-instagram"></i> Siga-nos no Instagram</a></li>
+            <li class="li-foot"><a href="https://www.youtube.com/@gremioestudantilldm" target="_blank"><i class="fab fa-youtube"></i> Assista aos nossos vídeos</a></li>
+        </ul>
     </div>
-    <ul id="footer-links-socials">
-        <li><a href="https://www.instagram.com/gremioestudantilldm/" target="_blank"><i class="fab fa-whatsapp"></i> Tire suas dúvidas</a></li>
-        <li><a href="https://www.instagram.com/gremio_estudantil.if/" target="_blank"><i class="fab fa-instagram"></i> Siga-nos no Instagram</a></li>
-        <li><a href="https://www.youtube.com/@gremioestudantilldm" target="_blank"><i class="fab fa-youtube"></i> Assista aos nossos vídeos</a></li>
-        <li>Email: <a href="mailto:"></a></li>
-    </ul>
     <h5><?php echo date('Y')?> &copy; Grêmio Estudantil - Legado de Malala. IFRS Campus Rolante.</h5>
 </footer>
